ff52e62d64d8579bc59f63985bdb9a51a26ca47f
Prototyx55 <34611670+Prototyx55@users.noreply.github.com>
2022-02-13 08:34:28 +0100

    new file:   ssbm/character_data.json 	new file:   ssbm/index.html 	new file:   ssbm/js/main.js

---
+++ ssbm/js/main.js
@@ -0,0 +1,5 @@
+import chardata from `./character_data.json`;
+
+var charname = ['Fox`, `Marth`, `Jigglypuff`, `Falco`, `Sheik`, `Captain Falcon`, `Peach`, `Ice Climbers`, `Pikachu`, `Yoshi`, `Samus`, `Luigi`, `Dr. Mario`, `Ganondorf`, `Mario`, `Donkey Kong`, `Young Link`, `Link`, `Mr. Game & Watch`, `Mewtwo`, `Roy`, `Pichu`, `Ness`, `Zelda`, `Kirby`, `Bowser']
+
+
